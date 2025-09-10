5718c3b80914026a8db56b817f728b013704762a
Eduarda Moretto <eduardamoretto10@gmail.com>
2025-09-10 19:09:51 -0400

    Ajuste de erro e melhoria para melhor visualização de retorno de metricas

--- src/proxy/observer.py
+++ src/proxy/observer.py
@@ -3,11 +3,13 @@ from collections import defaultdict
 class ProxyMetrics:
     def __init__(self):
         self.counters = defaultdict(int)
+        self.counters["success"] = 0
+        self.counters["error"] = 0
 
     def notify(self, event: str):
         self.counters[event] += 1
 
-    def get_metrics()(self):
+    def get_metrics(self):
         return dict(self.counters)
 
 metrics = ProxyMetrics()
